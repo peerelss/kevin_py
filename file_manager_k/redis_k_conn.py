0fc211ee48b3bd4f0a204036b00549fe50afcd96
kevin.xie <xiepeerless@gmail.com>
2022-02-15 22:30:25 -0800

    重构了tumblr文件存储的redis方式，从url改为文件名。

--- file_manager_k/redis_k_conn.py
+++ file_manager_k/redis_k_conn.py
@@ -1,6 +1,9 @@
 import redis
 import os
 
+r_redis = redis.Redis(host='localhost', port=6379, decode_responses=True)
+redis_str_tumblr_file_name_k_conn = 'redis_str_tumblr_file_name_k_conn'
+redis_deplicate_file_count_python = 'redis_deplicate_file_count_python'
 '''
 set_name = 'redis_file_dir_history'  # 记录已经扫描过得文件夹历史
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
@@ -11,9 +14,48 @@ file_name = '/media/kevin/My Passport/movies/北条麻妃/empty'
 str_end = os.path.splitext(file_name)[-1][1:]
 print(str_end)
 '''
-filepath = '/media/kevin/My Passport'
-if os.path.isdir(filepath):
-    for root, files_, dirs_ in os.walk(filepath):
-        if not os.listdir(root):
-            print(root)
-            os.rmdir(root)
+filepath = '/media/kevin/Backup/images/'
+
+
+def delete_the_same_file():
+    if os.path.isdir(filepath):
+        for i_file in os.listdir(filepath):
+            # print(i_file)
+            i_file_full = os.path.join(filepath, i_file)
+            if os.path.isdir(i_file_full):
+                for ii_file in os.listdir(i_file_full):
+                    str_ii_file = str(ii_file)  # 文件名
+                    ii_file_full = str(os.path.join(i_file_full, ii_file))  # 文件路径
+                    if 'tumblr' in str_ii_file:
+                        if r_redis.exists(str_ii_file):
+                            if r_redis.get(str_ii_file) == ii_file_full:
+                                pass
+                            else:
+                                print('different one')
+                                print(ii_file_full)
+                                print(r_redis.get(str_ii_file))
+                                if os.path.exists(r_redis.get(str_ii_file)):
+                                    print('exists')
+                                    r_redis.incr(redis_deplicate_file_count_python)
+                                    os.remove(ii_file_full)
+
+                        else:
+                            r_redis.set(str_ii_file, ii_file_full)
+
+
+# /media/kevin/Backup/images/61zonevacbed/tumblr_od1d2frNia1rrho82o1_1280.jpg
+def delete_test():
+    str_key = 'tumblr_od1d2frNia1rrho82o1_1280.jpg'
+    if r_redis.exists(str_key):
+        str = r_redis.get(str_key)
+        print(str)
+        if str == '/media/kevin/Backup/images/61zonevacbed/tumblr_od1d2frNia1rrho82o1_1280.jpg':
+            print('same one')
+        else:
+            print('different one')
+
+
+if __name__ == "__main__":
+    #delete_the_same_file()
+    # delete_test()
+    print(r_redis.get('redis_set_tumblr_dir_file_redirected_incr'))
--- file_manager_k/tumblr_images_downloader.py
+++ file_manager_k/tumblr_images_downloader.py
@@ -18,7 +18,7 @@ url_target = '/media/kevin/Backup/images/'
 r_redis = redis.Redis(host='localhost', port=6379, decode_responses=True)
 redis_tumblr_dir = "redis_set_tumblr_dir"  # 保存所有已经下载过的tumblr的txt的文件名
 redis_tumblr_dir_file_redirected = 'redis_set_tumblr_dir_file_redirected'  # 保存所有被重定向的url
-redis_tumblr_dir_file = 'redis_set_tumblr_dir_file'  # 保存所有已被下载的url
+redis_tumblr_dir_file_from_url = 'redis_set_tumblr_dir_file'  # 保存所有已被下载的url
 redis_tumblr_dir_file_redirected_incr = 'redis_set_tumblr_dir_file_redirected_incr'  # 记录多少个被重定向
 
 
@@ -45,26 +45,30 @@ def get_url_from_file(t_tumblr):
 
 
 def download_tumblr_jpg(*jpg_url):
-    file_name_t = str(jpg_url[0]).split('/')[-1]
+    file_name_t = str(jpg_url[0]).split('/')[-1]  # 文件名
     file_name_full = url_target + jpg_url[1] + '/' + file_name_t
-    if r_redis.sismember(redis_tumblr_dir_file, jpg_url[0]):
+    if r_redis.exists(file_name_t) and os.path.exists(r_redis.get(file_name_t)):
+        print('%s 存在 from disk ' % file_name_t)
+    elif r_redis.sismember(redis_tumblr_dir_file_from_url, file_name_t):
         print('%s 经被下载过 ' % jpg_url[0])
-    elif r_redis.sismember(redis_tumblr_dir_file_redirected, jpg_url[0]):
+    elif r_redis.sismember(redis_tumblr_dir_file_redirected, file_name_t):
         print('%s  已经被重定向 from redis' % jpg_url[0])
         r_redis.incr(redis_tumblr_dir_file_redirected_incr)
-    elif os.path.exists(file_name_full):
+    elif os.path.exists(file_name_full) or os.path.exists(
+            file_name_full.replace('daddywantskitten0', 'daddywantskitten')):
         print('%s 已存在' % jpg_url[0])
     else:
         html = requests.get(jpg_url[0])
         if html.history:
             print('%s  已经被重定向 from network' % jpg_url[0])
             r_redis.incr(redis_tumblr_dir_file_redirected_incr)
-            r_redis.sadd(redis_tumblr_dir_file_redirected, jpg_url[0])
+            r_redis.sadd(redis_tumblr_dir_file_redirected, file_name_t)
         else:
             with open(file_name_full, "wb") as file:
                 file.write(html.content)
             print('%s  正在下载ing  from network' % jpg_url[0])
-            r_redis.sadd(redis_tumblr_dir_file, file_name_full)
+            r_redis.sadd(redis_tumblr_dir_file_from_url, file_name_t)
+            r_redis.set(file_name_t, file_name_full)
 
 
 def get_file_length(file):
